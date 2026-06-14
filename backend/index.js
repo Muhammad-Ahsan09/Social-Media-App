0eb0930e0c51357ed6089337c3418bc5e26f589f
Venom <mmahsan322@gmail.com>
2026-06-14 11:54:21 -0700

    added frontedn url in cors origin

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ const upload = multer({storage})
 
 
 app.use(cors({
-    origin: "https://social-media-app-d66c.vercel.app/",
+    origin: "https://social-media-app-d66c.vercel.app",
     credentials: true
 }))
 
