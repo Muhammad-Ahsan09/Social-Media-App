49b95d96a85c0d4b74e686270f1c57d2a40445e5
Venom <mmahsan322@gmail.com>
2026-06-17 07:12:33 -0700

    added topnavbar

---
+++ frontend/vite-project/src/components/TopNavbar.jsx
@@ -0,0 +1,39 @@
+const TopNavbar = () => {
+
+    const handleLogout = async (e) => {
+        e.preventDefault()
+
+        await fetch("https://social-media-app-five-rust.vercel.app/api/auth/logout", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            credentials: "include"
+        })
+
+        setUser(false)
+        // navigate("/login")
+
+        
+    }
+    return (
+        <div className="md:hidden fixed top-0 left-0 w-full h-14 bg-white dark:bg-black border-b dark:border-gray-800 flex items-center justify-between px-4 z-50">
+
+            {/* App Name */}
+            <div className="font-bold text-lg">
+                GET SOCIAL
+            </div>
+
+            {/* Logout Button */}
+            <button
+                onClick={handleLogout}
+                className="bg-slate-200 dark:bg-gray-800 px-3 py-1 rounded text-sm font-medium"
+            >
+                Logout
+            </button>
+
+        </div>
+    );
+}; 
+
+export default TopNavbar
--- frontend/vite-project/src/pages/Home.jsx
+++ frontend/vite-project/src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import LeftSidebar from "../components/LeftSidebar";
 import PostsSection from "../components/PostsSection";
 import RightSidebar from "../components/RightSidebar";
-import BottomNavbar  from "../components/BottomNavbar";
+import BottomNavbar from "../components/BottomNavbar";
+import TopNavbar from "../components/TopNavbar";
 
 const Home = ({ setUser, currentUserId }) => {
     return <>
+        <TopNavbar setUser={setUser} />
         <div className="flex flex-col md:flex-row">
+
             <LeftSidebar setUser={setUser} currentUserId={currentUserId} />
             <PostsSection />
             <RightSidebar />
         </div>
 
-        <BottomNavbar currentUserId={currentUserId}  />
+        <BottomNavbar currentUserId={currentUserId} />
     </>
 }
 
