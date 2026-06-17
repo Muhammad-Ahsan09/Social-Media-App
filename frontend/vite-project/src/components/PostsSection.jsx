4ed5ceac078f4d514aeb5f0ade81ed2a86502b7c
Venom <mmahsan322@gmail.com>
2026-06-17 07:17:52 -0700

    added topnavbar

--- frontend/vite-project/src/components/PostsSection.jsx
+++ frontend/vite-project/src/components/PostsSection.jsx
@@ -2,7 +2,7 @@ import StoriesSection from "./StoriesSection"
 import Posts from "./Posts"
 
 const PostsSection = () => {
-    return <div className="w-full md:w-3/5 pb-16 md:pb-0">
+    return <div className="w-full md:w-3/5 pt-14 md:pt-0 pb-16 md:pb-0">
         {/* <StoriesSection/> */}
         <Posts/>
     </div>
